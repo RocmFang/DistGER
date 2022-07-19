e2b76b973cdcb1b96a10a58a9aee82bcf11bb454
Rocman <47979640+RocmFang@users.noreply.github.com>
2022-07-19 12:51:07 +0800

    Add files via upload

---
+++ distger.cmake
@@ -0,0 +1,19 @@
+function(add_app_exec EXEC_NAME)
+    include_directories(/opt/intel/oneapi/mkl/2022.0.2/include/)
+    link_directories(/opt/intel/oneapi/mkl/2022.0.2/lib/intel64 )
+    add_executable(${EXEC_NAME} ${EXEC_NAME}.cpp)
+     target_link_libraries(${EXEC_NAME} PUBLIC ${MPI_LIBRARIES}  -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl)
+    #target_link_libraries(${EXEC_NAME} PUBLIC ${MPI_LIBRARIES} -mkl=sequential)
+endfunction(add_app_exec)
+
+# function(add_test_exec EXEC_NAME)
+#     add_executable(${EXEC_NAME} ${EXEC_NAME}.cpp)
+#     target_link_libraries(${EXEC_NAME} PUBLIC ${GTEST_LIBRARIES} ${MPI_LIBRARIES})
+# endfunction(add_test_exec)
+
+function(add_tool_exec EXEC_NAME)
+    add_executable(${EXEC_NAME} ${EXEC_NAME}.cpp)
+    target_link_libraries(${EXEC_NAME} PUBLIC ${MPI_LIBRARIES})
+endfunction(add_tool_exec)
+
+
